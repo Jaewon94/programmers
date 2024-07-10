28b3afc9400b48e6757fa621238262ba5bb36207
Jaewon Kim <104521276+Jaewon94@users.noreply.github.com>
2024-07-10 09:16:07 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int[] num_list, int n) {
+        List list = new ArrayList();
+    
+        for (int i = 0; i<num_list.length; i++) {
+            if (i % n == 0) {
+                list.add(num_list[i]);
+            }
+        }
+        
+        
+        
+        return list;
+    }
+}
