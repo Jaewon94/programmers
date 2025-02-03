d89cad492ebe62f4fa6bd0a703abb208639a663a
Jaewon Kim <104521276+Jaewon94@users.noreply.github.com>
2025-02-03 15:46:07 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.57 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int size = Math.max(arr.length, arr[0].length);
+        String where = "";
+        if (arr.length > arr[0].length) {
+            where = "left";
+        } else if (arr.length < arr[0].length) {
+            where = "right";
+        } else {
+            where = "equal";
+        }
+        int[][] answer = new int[size][size];
+        int[] temp = new int[size];
+        
+        if (where.equals("left")) {
+            for(int i=0; i < arr.length; i++) {
+                for (int j=0; j<arr.length; j++) {
+                    answer[i][j] = j < arr[i].length ? arr[i][j] : 0;
+                }
+            }
+        } else if (where.equals("right")) {
+            for (int i=0; i< arr.length; i++) {
+                for(int j=0; j<arr[i].length; j++) {
+                    answer[i][j] = j < arr[i].length ? arr[i][j] : 0;
+                }
+            }
+        } else {
+            answer = arr;
+        }
+        return answer;
+    }
+}
